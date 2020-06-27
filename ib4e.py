13762b6a80f0c69207f2d9a262894dc13fa73f67
Nicholas Alderman <nicholas@Nicholass-MacBook-Pro-4.local>
2020-06-27 00:02:03 -0400

    Added Histogram

--- ib4e.py
+++ ib4e.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # pass in string - return true if string contains ei or ie
 def adjacent(string):
@@ -22,6 +24,7 @@ def cRule(string):
 wordsdf = pd.read_csv('words.txt', sep='\n', skiprows=1, names=['Words'])
 wordsdf['Words'] = wordsdf['Words'].astype(str)
 
+# only want words with ie/ei
 wordsdf['Adjacent'] = list(map(adjacent, wordsdf['Words']))
 wordsdf = wordsdf.dropna()
 # get number of each type of eiie
@@ -36,3 +39,26 @@ numctrue = sum(wordsdf.C_Rule == True)
 numcfalse = sum(wordsdf.C_Rule == False)
 
 pctrue = numctrue / (numctrue+numcfalse)
+
+# histogram - length of word vs how many words follow rule
+# x-axis: length of word    y-axis: num of followers
+# get length of word column
+wordsdf['Length'] = wordsdf.Words.str.len()
+# separate dataframes for true and false (for plotting purposes)
+truedf = wordsdf[(wordsdf.C_Rule == True) & (wordsdf.Length <= 20)]
+falsedf = wordsdf[(wordsdf.C_Rule == False) & (wordsdf.Length <= 20)]
+
+
+plt.hist(x=[truedf.Length,falsedf.Length],
+                            bins=np.arange(3,21,1),
+                            color=['#0504aa','#d12115'],
+                            alpha=0.7,
+                            rwidth=0.85,
+                            align='right')
+plt.xlabel('Length of Word')
+plt.xticks(np.arange(3,21,1))
+plt.ylabel('Frequency')
+plt.yticks([0,1000,2500])
+plt.title('I before E rule followers based on length of word')
+plt.legend(['Rule Followers','Rule Exceptions'])
+plt.show()
